f3202b548aeaaf5b7d2871d5153578ffbba7ce57
N33RD <kivim2018@gmail.com>
2021-02-22 15:20:45 -0300

    Moving Pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -22,7 +24,28 @@ public class ChessMatch {
 		}
 		return mat;
 	}
-
+	
+	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targPosition) {
+		Position source = sourcePosition.toPosition(); 
+		Position target = targPosition.toPosition();
+		validateSourcePsoition(source);
+		Piece capturedpiece = makeMove(source, target);
+		return 	(ChessPiece)capturedpiece;	
+	}
+	
+	private Piece makeMove(Position source, Position target) {
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.placePiece(p, target);
+		return capturedPiece;
+	}
+	
+	private void validateSourcePsoition(Position position) {
+		if(!board.thereIsAPiece(position)) {
+			throw new ChessException("There is no piece on source position");
+		}
+	}
+	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
